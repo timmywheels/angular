9986a7320c4ff8bf9944e2d1d28e3957a1c1b2f2
Tim Wheeler <timothy.wheeler@virginpulse.com>
2019-08-07 15:25:48 -0400

    init

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created...';
+  serverName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server created...';
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log('event:', event);
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+
+}
